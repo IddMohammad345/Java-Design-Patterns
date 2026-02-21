59d20c80d4ca5166c97fa1c5cb8271373b5672d7
Idd mohammad <mohammadidd345@gmail.com>
2026-02-21 14:45:45 +0530

    templete Pattern DataRenderer

---
+++ corePatterns/src/com/idd/patterns/templatemethod/CSVDataRenderer.java
@@ -0,0 +1,15 @@
+package com.idd.patterns.templatemethod;
+
+public class CSVDataRenderer extends DataRenderer {
+
+	@Override
+	public String readData() {
+		return "CSV Data";
+	}
+
+	@Override
+	public String processData(String data) {
+		return "Processed"+data;
+	}
+
+}
---
+++ corePatterns/src/com/idd/patterns/templatemethod/Test.java
@@ -0,0 +1,10 @@
+package com.idd.patterns.templatemethod;
+
+public class Test {
+
+	public static void main(String[] args) {
+		DataRenderer dataRenderer = new XMLDataRenderer();
+		dataRenderer.render();
+	}
+
+}
